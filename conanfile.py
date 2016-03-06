f82bad1a2e77d3de19ba3e351747cc6b8dce086b
Sam Spilsbury <smspillaz@gmail.com>
2016-03-06 13:06:29 +0800

    conan: Track latest version starting at 0.0.1

--- conanfile.py
+++ conanfile.py
@@ -2,24 +2,28 @@ from conans import ConanFile
 from conans.tools import download, unzip
 import os
 
+VERSION = "0.0.1"
 
-class CMakeForwardArgumentsConan(ConanFile):
+
+class CMakeForwardArguments(ConanFile):
     name = "cmake-forward-arguments"
-    version = "master"
-    generators = "cmake"
+    version = os.environ.get("CONAN_VERSION_OVERRIDE", VERSION)
     requires = ("cmake-include-guard/master@smspillaz/cmake-include-guard", )
+    generators = "cmake"
     url = "http://github.com/polysquare/cmake-forward-arguments"
-    license = "MIT"
+    licence = "MIT"
 
     def source(self):
-        zip_name = "cmake-forward-arguments-master.zip"
-        download("https://github.com/polysquare/" +
-                 "cmake-forward-arguments/archive/master.zip", zip_name)
+        zip_name = "cmake-forward-arguments.zip"
+        download("https://github.com/polysquare/"
+                 "cmake-forward-arguments/archive/{version}.zip"
+                 "".format(version="v" + VERSION),
+                 zip_name)
         unzip(zip_name)
         os.unlink(zip_name)
 
     def package(self):
         self.copy(pattern="*.cmake",
                   dst="cmake/cmake-forward-arguments",
-                  src=".",
+                  src="cmake-forward-arguments-" + VERSION,
                   keep_path=True)
